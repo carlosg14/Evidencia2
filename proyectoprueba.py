ad8b0f3e55678bbed91ceb7886bba83e498439b4
Felix Tapia <felixcruztapia@gmail.com>
2024-06-05 16:08:26 -0300

    Archivo con las funciones.

--- proyectoprueba.py
+++ proyectoprueba.py
@@ -1,37 +1,40 @@
-def reservar_aula():
-    # Opcion de reserva
-    print("Agendando reserva...")
+import CRUD
 
-def cancelar_reserva():
-    # Opcion de cancelacion
-    print("Cancelando reserva...")
-
-def ver_reserva():
-    # Opcion de observacion de reserva
-    print("Mostrando reserva...")
 
 def main():
+    """
+    En la funcion principal se presenta por consola el menu de opciones al usuario y
+    en base a la eleccion del mismo se llama al procedimiento correspondiente.
+
+    Retornos:
+
+        [None]: Esta funcion no retorna nada.
+    """
     while True:
+
+        # Impresion del menu.
+        print('=' * 30)
         print("Menú:")
         print("1) Reservar aula")
         print("2) Cancelar reserva")
         print("3) Ver reserva")
         print("4) Salir")
+        print('=' * 30)
         opcion = input("Ingrese el número de la opción que desea realizar: ")
 
+        # Llamada a la funcion correspondiente.
         if opcion == "1":
-            reservar_aula()
-            print ("su aula fue reservada con exito!")
+            CRUD.reservar_aula()
         elif opcion == "2":
-            cancelar_reserva()
-            print ("su reserva fue cancelada")
+            CRUD.cancelar_reserva()
         elif opcion == "3":
-            ver_reserva()
-            print ("procesando su reserva")
+            CRUD.ver_reservas()
         elif opcion == "4":
             print("¡Hasta luego!")
             break
         else:
             print("Opción no válida. Por favor, ingrese un número del 1 al 4.")
+
+
 if __name__ == "__main__":
     main()
